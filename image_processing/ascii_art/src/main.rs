8f17ac1499e03e4f8045bd6c18656efdbee2ce53
yasuomaidana <yasuomaidana@gmail.com>
2024-07-22 16:54:27 +0900

    Loading a color scaled image

--- image_processing/ascii_art/src/main.rs
+++ image_processing/ascii_art/src/main.rs
@@ -1,8 +1,13 @@
+use crate::reader_writer::read_image_single_channel;
+
 mod reader_writer;
 mod error;
 mod prelude;
-mod ColorScale;
+mod color_scale;
 
 fn main() {
     println!("Hello, world!");
+    let scaled_image =  read_image_single_channel("Sign.jpg",
+                                                  &color_scale::ColorScale::new(0.2989, 0.587, 0.114));
+
 }
