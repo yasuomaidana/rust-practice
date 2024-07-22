bbaa07b68194d049d5c8847a5023d82f28752996
yasuomaidana <yasuomaidana@gmail.com>
2024-07-22 17:33:51 +0900

    Printing char art image

--- image_processing/ascii_art/src/main.rs
+++ image_processing/ascii_art/src/main.rs
@@ -1,13 +1,29 @@
-use crate::reader_writer::read_image_single_channel;
+use crate::ascii_painter::{reduce_image_by_sampling, to_ascii_image};
+use crate::error::Error;
+use crate::reader_writer::{read_image_single_channel, save_img};
 
 mod reader_writer;
 mod error;
 mod prelude;
 mod color_scale;
+mod kernel;
+mod ascii_painter;
 
-fn main() {
-    println!("Hello, world!");
-    let scaled_image =  read_image_single_channel("Sign.jpg",
-                                                  &color_scale::ColorScale::new(0.2989, 0.587, 0.114));
+fn main() -> Result<(), Error> {
+    let (width, height, color_scaled_image) =  read_image_single_channel("Sign.jpg",
+                                                                         &color_scale::ColorScale::new(0.2989, 0.587, 0.114))?;
 
+    let reduced_image = reduce_image_by_sampling(&color_scaled_image, 10);
+    let (reduced_width, reduced_height) = (reduced_image[0].len(), reduced_image.len());
+    save_img(reduced_width as u32, reduced_height as u32, "reduced.jpg", &reduced_image)?;
+
+    let mut ascii_image = to_ascii_image(&reduced_image);
+
+    for row in ascii_image {
+        for pixel in row {
+            print!("{}", pixel);
+        }
+        println!();
+    }
+    Ok(())
 }
