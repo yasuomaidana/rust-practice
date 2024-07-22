fef33a9b91c6f0db963e438be348d86791ea4e8f
yasuomaidana <yasuomaidana@gmail.com>
2024-07-22 19:26:20 +0900

    Uniform scaling

--- image_processing/ascii_art/src/ascii_painter.rs
+++ image_processing/ascii_art/src/ascii_painter.rs
@@ -14,12 +14,12 @@ pub fn reduce_image_by_sampling(image: &Vec<Vec<f64>>, scale: usize) -> Vec<Vec<
     let original_width = if !image.is_empty() { image[0].len() } else { 0 };
 
     // Calculate new dimensions based on the sampling condition
-    let new_height = original_height / scale ;
+    let new_height = original_height / scale;
     let new_width = original_width / scale;
 
     let mut reduced_image = vec![vec![0.0; new_width]; new_height];
 
-    for (new_y, y) in (0..original_height).filter(|&y| y % (scale * 2) == 0).enumerate() {
+    for (new_y, y) in (0..original_height).filter(|&y| y % scale  == 0).enumerate() {
         for (new_x, x) in (0..original_width).filter(|&x| x % scale == 0).enumerate() {
             if new_y < new_height && new_x < new_width {
                 reduced_image[new_y][new_x] = image[y][x];
