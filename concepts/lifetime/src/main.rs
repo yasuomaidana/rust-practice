32add6b2ae4afec9fb06b720dbc42878a4e68f64
yasuomaidana <yasuomaidana@gmail.com>
2024-07-25 18:03:18 +0900

    Starting lifetime practice

--- concepts/lifetime/src/main.rs
+++ concepts/lifetime/src/main.rs
@@ -1,3 +1,30 @@
+fn example_2(){
+    let foo = 69;
+    let mut r;
+    {
+        let bar = 42;
+        r = &bar;
+        println!("r: {}", r);
+        println!("r: {}", *r);
+    }
+    r = &foo;
+    println!("r: {}", r);
+
+}
+
+// Both s1 and s2 are references which have the same lifetime 'a
+fn longest<'a>(s1:&'a str, s2:&'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    example_2();
+    let s1 = String::from("abcd");
+    let s2 = "xyz";
+    let result = longest(&s1, s2);
+    println!("The longest string is {}", result);
 }
