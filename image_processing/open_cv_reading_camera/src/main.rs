695d17900063620406a6f9702e6844632a43bae6
yasuomaidana <yasuomaidana@gmail.com>
2024-07-26 12:10:03 +0900

    Getting opencv runnable

--- image_processing/open_cv_reading_camera/src/main.rs
+++ image_processing/open_cv_reading_camera/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{
+    Result,
+    prelude::*,
+    videoio,
+    highgui
+};
+
+fn main() -> Result<()> {
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    highgui::named_window("video", highgui::WINDOW_FULLSCREEN)?;
+    let mut frame = Mat::default();
+
+    loop {
+        cam.read(&mut frame)?;
+        highgui::imshow("video", &frame)?;
+        let key = highgui::wait_key(10)?;
+        if key == 113 {
+            break;
+        }
+    }
+    Ok(())
 }
