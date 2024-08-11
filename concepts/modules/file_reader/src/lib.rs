46302b7fb2154a4bd3f907e174e1fa1d2c96a81c
yasuomaidana <yasuomaidana@gmail.com>
2024-08-11 20:01:01 +0900

    Using modules and reading a file

--- concepts/modules/file_reader/src/lib.rs
+++ concepts/modules/file_reader/src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs;
+use std::io::BufRead;
+
+pub struct TextFile {
+    file_name: String,
+    lines: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl TextFile {
+    pub fn new(file_name: &str) -> TextFile {
+        let lines = _read_file(file_name);
+        TextFile {
+            file_name: file_name.to_string(),
+            lines,
+        }
     }
+
+    pub fn get_lines(&self) -> &Vec<String> {
+        &self.lines
+    }
+
+    pub fn get_file_name(&self) -> &str {
+        &self.file_name
+    }
+
+    pub fn read_from_console() -> TextFile {
+        let mut input = String::new();
+        println!("Enter file name: ");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        TextFile::new(input.trim())
+    }
+
 }
+
+fn _read_file(file_name: &str) -> Vec<String> {
+    fs::read(file_name)
+        .expect("Unable to read file")
+        .lines()
+        .map(|line| line.unwrap())
+        .collect()
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
--- concepts/modules/src/main.rs
+++ concepts/modules/src/main.rs
@@ -1,3 +1,10 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let file = file_reader::TextFile::read_from_console();
+    let lines = file.get_lines();
+    for line in lines {
+        println!("{}", line);
+    }
+    println!("Read file {}", file.get_file_name());
 }
