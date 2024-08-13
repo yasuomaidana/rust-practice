e9278aa2c57c8a8fad62fd429c418516351605f5
yasuomaidana <yasuomaidana@gmail.com>
2024-08-13 22:04:11 +0900

    Adding color formatter

--- concepts/modules/file_reader/src/color_text.rs
+++ concepts/modules/file_reader/src/color_text.rs
@@ -16,5 +16,58 @@
 /// println!("{}", green_text); // This will print "Hello, world!" in green color.
 /// ```
 pub fn green(text: &str) -> String {
-    format!("\x1b[32m{}\x1b[0m", text)
+    formatter(Color::Green, text)
+}
+
+pub fn red(text: &str) -> String {
+    formatter(Color::Red, text)
+}
+
+pub fn yellow(text: &str) -> String {
+    formatter(Color::Yellow, text)
+}
+
+pub fn blue(text: &str) -> String {
+    formatter(Color::Blue, text)
+}
+
+pub fn magenta(text: &str) -> String {
+    formatter(Color::Magenta, text)
+}
+
+pub fn cyan(text: &str) -> String {
+    formatter(Color::Cyan, text)
+}
+
+pub fn white(text: &str) -> String {
+    formatter(Color::White, text)
+}
+
+pub enum Color {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    Default,
+}
+
+fn formatter(color: Color, text: &str) -> String {
+    let code = match color {
+        Color::Red => 31,
+        Color::Green => 32,
+        Color::Yellow => 33,
+        Color::Blue => 34,
+        Color::Magenta => 35,
+        Color::Cyan => 36,
+        Color::White => 37,
+        Color::Default => 0,
+    };
+    format!("\x1b[{}m{}\x1b[0m", code, text)
+}
+
+pub fn reset(text: &str) -> String {
+    formatter(Color::Default, text)
 }
--- concepts/modules/src/main.rs
+++ concepts/modules/src/main.rs
@@ -1,10 +1,20 @@
-use file_reader::color_text::green;
+use file_reader::color_text::{blue, cyan, green, magenta, red, white, yellow};
 
 fn main() {
     let file = file_reader::TextFile::read_from_console();
     let lines = file.get_lines();
-    for line in lines {
-        println!("{}", line);
+    let colors = vec![
+        green,
+        red,
+        yellow,
+        blue,
+        magenta,
+        cyan,
+        white
+    ];
+    for (i, line) in lines.iter().enumerate() {
+        let color = colors[i % colors.len()];
+        println!("{}",color(line));
     }
     println!("Read file {}", green(file.get_file_name()));
 }
