783a5ab6b63b4e82004819f73a36e240377a86f1
yasuomaidana <yasuomaidana@gmail.com>
2024-08-19 20:01:20 +0900

    Using clap to read variables

--- playing/cli-practice/src/main.rs
+++ playing/cli-practice/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli{
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Using input variables");
+    println!("{:?}", args);
+
+    let content = fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            let line = line.replace(&args.pattern, &format!("\x1b[0;31m{}\x1b[0m", &args.pattern));
+            println!("{}", line);
+        }
+    }
 }
