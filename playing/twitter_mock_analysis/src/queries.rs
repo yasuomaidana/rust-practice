827259ce967874937c2cc8e09e141eb16c5a9c9e
yasuomaidana <yasuomaidana@gmail.com>
2024-08-26 15:55:22 +0900

    Retrieving user as optional string

--- playing/twitter_mock_analysis/src/queries.rs
+++ playing/twitter_mock_analysis/src/queries.rs
@@ -21,7 +21,7 @@ impl FakeTwitterDatabase {
         }
     }
 
-    pub async fn get_user(&self, username: &str) -> Option<neo4rs::Row> {
+    pub async fn get_user(&self, username: &str) -> Option<String> {
         let get_user_query = fs::read_to_string("./neo4j_definition/get_user.cypher").unwrap();
         let get_user_query = query(get_user_query.as_str());
         let mut result = self.graph.execute(get_user_query.param("username", username)).await.unwrap();
@@ -29,17 +29,12 @@ impl FakeTwitterDatabase {
         match user_stored {
             Ok(user) => {
                 match user {
-                    Some(user) => {
-                        Some(user)
-                    }
-                    None => {
-                        None
-                    }
+                    Some(user) => user.get("username").unwrap(),
+                    None => None
                 }
             }
             Err(e) => {
-                println!("Error: {:?}", e);
-                None
+                panic!("Error: {:?}", e);
             }
         }
     }
