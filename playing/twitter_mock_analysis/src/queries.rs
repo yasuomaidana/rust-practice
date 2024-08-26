b15453635ae66b08640d5f2406fe2d00fee07fc6
yasuomaidana <yasuomaidana@gmail.com>
2024-08-26 16:59:02 +0900

    Creating user cypher

--- playing/twitter_mock_analysis/src/queries.rs
+++ playing/twitter_mock_analysis/src/queries.rs
@@ -1,8 +1,17 @@
+use std::error::Error;
 use std::fs;
-use neo4rs::{query, ConfigBuilder, Graph};
+use neo4rs::{query, ConfigBuilder, Graph, Keys, Labels, Neo4jError, Node};
+use neo4rs::Error::Neo4j;
 
 pub(crate) struct FakeTwitterDatabase{
     graph: Graph
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub(crate) struct  User{
+    labels: Labels,
+    keys: Keys<Vec<String>>,
+    username: String,
 }
 
 impl FakeTwitterDatabase {
@@ -21,7 +30,7 @@ impl FakeTwitterDatabase {
         }
     }
 
-    pub async fn get_user(&self, username: &str) -> Option<String> {
+    pub async fn get_user(&self, username: &str) -> Option<User> {
         let get_user_query = fs::read_to_string("./neo4j_definition/get_user.cypher").unwrap();
         let get_user_query = query(get_user_query.as_str());
         let mut result = self.graph.execute(get_user_query.param("username", username)).await.unwrap();
@@ -29,7 +38,7 @@ impl FakeTwitterDatabase {
         match user_stored {
             Ok(user) => {
                 match user {
-                    Some(user) => user.get("username").unwrap(),
+                    Some(user) => user.get("username").ok(),
                     None => None
                 }
             }
@@ -58,5 +67,16 @@ impl FakeTwitterDatabase {
                 panic!("Error: {:?}", e);
             }
         }
+    }
+
+    pub async fn create_user(&self, user:String)-> String{
+        let create_user_query = fs::read_to_string("./neo4j_definition/create_user.cypher").unwrap();
+        let create_user_query = query(create_user_query.as_str());
+        let mut result = self.graph.execute(create_user_query.param("username", user)).await.unwrap();
+
+        let Ok(Some(user)) = result.next().await else {panic!("Error creating user")};
+
+        let node:Node = user.get("user").unwrap();
+        node.get::<String>("username").unwrap()
     }
 }
