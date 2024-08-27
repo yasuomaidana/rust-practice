0c2bfeedb4bc98d193dcdfd62b5ff0702c33a3af
yasuomaidana <yasuomaidana@gmail.com>
2024-08-27 15:43:16 +0900

    Normalizing models

--- image_processing/open_cv_reading_camera/src/mnist_model.rs
+++ image_processing/open_cv_reading_camera/src/mnist_model.rs
@@ -62,8 +62,24 @@ pub struct ConvNet {
     dropout: candle_nn::Dropout,
 }
 
-impl ConvNet {
-    pub(crate) fn new(vs: VarBuilder) -> Result<Self> {
+impl ConvNet{
+    fn forward_training(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
+        let (b_sz, _img_dim) = xs.dims2()?;
+        let xs = xs
+            .reshape((b_sz, 1, 28, 28))?
+            .apply(&self.conv1)?
+            .max_pool2d(2)?
+            .apply(&self.conv2)?
+            .max_pool2d(2)?
+            .flatten_from(1)?
+            .apply(&self.fc1)?
+            .relu()?;
+        self.dropout.forward_t(&xs, train)?.apply(&self.fc2)
+    }
+}
+
+impl Model for ConvNet {
+    fn new(vs: VarBuilder) -> Result<Self> {
         let conv1 = candle_nn::conv2d(1, 32, 5, Default::default(), vs.pp("c1"))?;
         let conv2 = candle_nn::conv2d(32, 64, 5, Default::default(), vs.pp("c2"))?;
         let fc1 = candle_nn::linear(1024, 1024, vs.pp("fc1"))?;
@@ -78,18 +94,8 @@ impl ConvNet {
         })
     }
 
-    fn forward(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
-        let (b_sz, _img_dim) = xs.dims2()?;
-        let xs = xs
-            .reshape((b_sz, 1, 28, 28))?
-            .apply(&self.conv1)?
-            .max_pool2d(2)?
-            .apply(&self.conv2)?
-            .max_pool2d(2)?
-            .flatten_from(1)?
-            .apply(&self.fc1)?
-            .relu()?;
-        self.dropout.forward_t(&xs, train)?.apply(&self.fc2)
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        self.forward_training(xs, false)
     }
 }
 
@@ -136,7 +142,7 @@ pub fn training_loop_cnn(
         for batch_idx in batch_idxs.iter() {
             let train_images = train_images.narrow(0, batch_idx * BSIZE, BSIZE)?;
             let train_labels = train_labels.narrow(0, batch_idx * BSIZE, BSIZE)?;
-            let logits = model.forward(&train_images, true)?;
+            let logits = model.forward_training(&train_images, true)?;
             let log_sm = ops::log_softmax(&logits, D::Minus1)?;
             let loss = loss::nll(&log_sm, &train_labels)?;
             opt.backward_step(&loss)?;
@@ -144,7 +150,7 @@ pub fn training_loop_cnn(
         }
         let avg_loss = sum_loss / n_batches as f32;
 
-        let test_logits = model.forward(&test_images, false)?;
+        let test_logits = model.forward_training(&test_images, false)?;
         let sum_ok = test_logits
             .argmax(D::Minus1)?
             .eq(&test_labels)?
