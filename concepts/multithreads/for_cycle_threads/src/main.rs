f201a28ff4c479e2d1e0aecf468e1a67bfdcac0c
yasuomaidana <yasuomaidana@gmail.com>
2024-09-28 14:10:33 -0600

    Using threads in rust

--- concepts/multithreads/for_cycle_threads/src/main.rs
+++ concepts/multithreads/for_cycle_threads/src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+
+const NUMBER_OF_THREADS: i32 = 10;
 fn main() {
-    println!("Hello, world for cycle!");
+    let mut threads = vec![];
+
+    for i in 0..NUMBER_OF_THREADS {
+        threads.push(thread::spawn(move || {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(std::time::Duration::from_millis(1));
+        }));
+    }
+    for thread in threads {
+        thread.join().unwrap();
+    }
 }
