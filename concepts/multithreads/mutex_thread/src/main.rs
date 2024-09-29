c96a553c0bd7f2a0ba8c5e51c490fab8af7abce7
yasuomaidana <yasuomaidana@gmail.com>
2024-09-28 19:56:45 -0600

    Using mutex with arc and threads

--- concepts/multithreads/mutex_thread/src/main.rs
+++ concepts/multithreads/mutex_thread/src/main.rs
@@ -1,18 +1,31 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
-    
-    let counter_clone = Arc::clone(&counter);
+    let num_threads = 10;
 
-    let handle = thread::spawn(move || {
-        let mut num = counter_clone.lock().unwrap();
-        *num += 1;
+    let threads = (0..num_threads).map(|_| {
+        let counter_clone = Arc::clone(&counter);
+        thread::spawn(move || {
+            sleep(Duration::from_millis(1000));
+            let counter = counter_clone.try_lock();
+            match counter {
+                Ok(mut num) => {
+                    *num += 1;
+                },
+                Err(_) => {
+                    println!("Thread failed to acquire lock");
+                }
+            }
+        })
     });
 
-    handle.join().expect("Thread panicked");
-    let a = counter.lock().unwrap();
+    threads.for_each(|t| {
+        t.join().unwrap();
+    });
 
-    println!("num: {}", a);
+    println!("Result: {}", *counter.lock().unwrap());
 }
