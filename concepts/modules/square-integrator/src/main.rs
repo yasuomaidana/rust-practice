bb8402855c2cd50a75d5add035d354320ce0d839
yasuomaidana <yasuomaidana@gmail.com>
2024-10-29 15:07:00 -0600

    Square integrator problem

--- concepts/modules/square-integrator/src/main.rs
+++ concepts/modules/square-integrator/src/main.rs
@@ -1,3 +1,11 @@
+use custom_maths::function_evaluator;
+
 fn main() {
-    println!("Hello, world!");
+
+    let distance_f = |x: f64| 4.0 * x - 0.2*x.powi(2)/2.0;
+    let seconds = 36.0;
+    let minutes =  15.0 + seconds / 60.0;
+    let hours = 8.0 + minutes / 60.0;
+    let distance = function_evaluator(distance_f, 0.0, hours);
+    println!("Traveled distance is {:.3} m", distance);
 }
