109c7d5db87a99cc77af91b8095e2a8ea62913b8
yasuomaidana <yasuomaidana@gmail.com>
2024-10-29 23:37:56 -0600

    Simulating data reading through text file

--- playing/manchester_encoding/reader/src/lib.rs
+++ playing/manchester_encoding/reader/src/lib.rs
@@ -1,3 +1,6 @@
+
+mod reader;
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
---
+++ playing/manchester_encoding/reader/src/reader.rs
@@ -0,0 +1,78 @@
+use crate::read_lines;
+
+trait ReadData {
+    fn read_data(&mut self) -> Option<char>;
+}
+
+struct FileReader {
+    string_buffer: Vec<String>,
+    current_chars: Vec<char>,
+    new_line: bool,
+    starting: bool,
+}
+
+impl FileReader {
+    fn new(file_name: String) -> Self {
+        FileReader {
+            string_buffer: read_lines(file_name).unwrap(),
+            current_chars: vec![],
+            new_line: false,
+            starting: true,
+        }
+    }
+}
+
+impl ReadData for FileReader {
+    fn read_data(&mut self) -> Option<char> {
+        if self.current_chars.len() == 0 {
+            if self.string_buffer.len() == 0 {
+                return None;
+            }
+            let line = self.string_buffer.remove(0);
+            self.current_chars = line.chars().collect();
+            self.new_line = true;
+        }
+        {
+            if self.new_line {
+                if self.starting {
+                    self.starting = false;
+                    self.new_line = false;
+                } else {
+                    self.new_line = false;
+                    return Some('\n');
+                }
+            }
+        }
+
+        Some(self.current_chars.remove(0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::path::Path;
+    use crate::delete_file;
+    use super::*;
+
+    #[test]
+    fn reading_text_file() {
+        if Path::new("text.txt").exists() {
+            delete_file("text.txt").expect("Unable to delete file");
+        }
+        fs::write("text.txt", "hello\nbye").expect("Unable to write file");
+
+        let mut file_reader = FileReader::new("text.txt".to_string());
+
+        assert_eq!(file_reader.read_data().unwrap(), 'h');
+        assert_eq!(file_reader.read_data().unwrap(), 'e');
+        assert_eq!(file_reader.read_data().unwrap(), 'l');
+        assert_eq!(file_reader.read_data().unwrap(), 'l');
+        assert_eq!(file_reader.read_data().unwrap(), 'o');
+        assert_eq!(file_reader.read_data().unwrap(), '\n');
+        assert_eq!(file_reader.read_data().unwrap(), 'b');
+        assert_eq!(file_reader.read_data().unwrap(), 'y');
+        assert_eq!(file_reader.read_data().unwrap(), 'e');
+        assert_eq!(file_reader.read_data(), None);
+    }
+}
