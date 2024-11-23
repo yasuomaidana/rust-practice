0d6e5a9cedb60a4d68adebdc3244cb4f4356ea8e
yasuomaidana <yasuomaidana@gmail.com>
2024-11-23 12:26:32 -0600

    Defining simple macro and macro with identifier

---
+++ concepts/custom-macros/src/macros.rs
@@ -0,0 +1,20 @@
+macro_rules! say_hello {
+    () => {
+        println!("Hello!");
+    };
+}
+
+// This macro takes an identifier as an argument and creates a
+// function with that name.
+macro_rules! create_function {
+    ($func_name:ident) => {
+        // The function, when called, prints a message indicating the function's name.
+        // The empty line you are selecting serves as a separator between these two macro
+        // definitions for better readability.
+        fn $func_name() {
+            // stringify! This macro will yield an expression of type &'static str 
+            // which is the stringification of all the tokens passed to the macro.
+            println!("You called {:?}()", stringify!($func_name));
+        }
+    };
+}
