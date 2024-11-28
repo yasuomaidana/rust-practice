22521ea1e0c0c5b92cb89aecdc1b569b434a6409
yasuomaidana <yasuomaidana@gmail.com>
2024-11-28 15:09:11 -0600

    Creating a post and get method with the same url path

--- concepts/rocket-practice/src/main.rs
+++ concepts/rocket-practice/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use std::time::Duration;
+use rocket::tokio::time::sleep;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[post("/delay/<seconds>")]
+async fn delay_post(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds post", seconds)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount(
+        "/", routes![index, delay, delay_post],
+    )
 }
