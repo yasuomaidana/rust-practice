8660deee50f8c6a91bac68c6b83d8d0bbb676c82
yasuomaidana <yasuomaidana@gmail.com>
2024-11-28 15:21:56 -0600

    Loading image from static direction

--- concepts/rocket-practice/src/main.rs
+++ concepts/rocket-practice/src/main.rs
@@ -1,7 +1,10 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use std::time::Duration;
+use rocket::tokio;
 use rocket::tokio::time::sleep;
+use std::path::PathBuf;
+use std::time::Duration;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -20,9 +23,27 @@ async fn delay_post(seconds: u64) -> String {
     format!("Waited for {} seconds post", seconds)
 }
 
+#[get("/image/<filename..>")]
+async fn image(filename: PathBuf) -> Option<tokio::fs::File> {
+    let path = PathBuf::from("static").join(filename);
+    if path.is_file() {
+        if let Some(ext) = path.extension() {
+            if ext == "png" || ext == "jpg" {
+                return Some(tokio::fs::File::open(path).await.unwrap());
+            }
+        }
+    }
+    None
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/", routes![index, delay, delay_post],
-    )
+    rocket::build()
+        .mount("/",
+               routes![
+                   index, 
+                   delay, 
+                   delay_post,
+                   image
+               ])
 }
