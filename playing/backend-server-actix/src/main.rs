380826e24e6c9a13d45f74ce01c8419d5b5f0d6c
yasuomaidana <yasuomaidana@gmail.com>
2024-12-01 13:50:26 -0600

    Serving static files

--- playing/backend-server-actix/src/main.rs
+++ playing/backend-server-actix/src/main.rs
@@ -1,8 +1,8 @@
-use std::future::Future;
-use std::path::PathBuf;
 use actix_files::NamedFile;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use log::info;
+use std::future::Future;
+use std::path::PathBuf;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -29,20 +29,35 @@ async fn app_index() -> actix_web::Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+async fn manual_index() -> actix_web::Result<NamedFile> {
+    let path = PathBuf::from("index.html");
+    Ok(NamedFile::open(path)?)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::builder().filter_level(log::LevelFilter::Info).init();
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
-            .route("/hey", web::get().to(|| func_wrapper("Hey there!".to_string())))
+            .route(
+                "/hey",
+                web::get().to(|| func_wrapper("Hey there!".to_string())),
+            )
             .service(
                 web::scope("/files")
                     .service(app_index)
+                    .route("", web::get().to(manual_index))
+                    /// Serve static files from the /public directory
+                    /// To access this file, go to http://localhost:8080/public/"filename"
+                    /// it will serve the file from the static directory
+                    .service(actix_files::Files::new("/public", "./static").show_files_listing())
             )
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
