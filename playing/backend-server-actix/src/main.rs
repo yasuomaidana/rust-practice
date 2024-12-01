d2637488e4beb7ca5804aec1ecb1aa009ad2b380
yasuomaidana <yasuomaidana@gmail.com>
2024-12-01 11:43:37 -0600

    Using function wrapper to return parametrized endpoints

--- playing/backend-server-actix/src/main.rs
+++ playing/backend-server-actix/src/main.rs
@@ -1,3 +1,4 @@
+use std::future::Future;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -10,8 +11,12 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn manual_hello(message: String) -> impl Responder {
+    HttpResponse::Ok().body(message)
+}
+
+fn func_wrapper() -> impl Future<Output=impl Responder> {
+    manual_hello("Hey there!".to_string())
 }
 
 #[actix_web::main]
@@ -20,7 +25,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hey", web::get().to(func_wrapper))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
