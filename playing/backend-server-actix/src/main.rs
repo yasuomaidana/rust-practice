da9a16a1e2446222e326b2a56819ef44305d0086
yasuomaidana <yasuomaidana@gmail.com>
2024-12-01 12:33:48 -0600

    Serving a single file using scope and empty "/" url

--- playing/backend-server-actix/src/main.rs
+++ playing/backend-server-actix/src/main.rs
@@ -1,5 +1,8 @@
 use std::future::Future;
+use std::path::PathBuf;
+use actix_files::NamedFile;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use log::info;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,6 +15,7 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 async fn manual_hello(message: String) -> impl Responder {
+    info!("Request received: {}", &message);
     HttpResponse::Ok().body(message)
 }
 
@@ -19,13 +23,24 @@ fn func_wrapper(message: String) -> impl Future<Output = impl Responder> {
     manual_hello(message)
 }
 
+#[get("/")]
+async fn app_index() -> actix_web::Result<NamedFile> {
+    let path = PathBuf::from("index.html");
+    Ok(NamedFile::open(path)?)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::builder().filter_level(log::LevelFilter::Info).init();
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(|| func_wrapper("Hey there!".to_string())))
+            .service(
+                web::scope("/files")
+                    .service(app_index)
+            )
     })
         .bind(("127.0.0.1", 8080))?
         .run()
