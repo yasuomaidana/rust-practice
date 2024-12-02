43e8057b70f984524f606b99717bfe4e9af893a7
yasuomaidana <yasuomaidana@gmail.com>
2024-12-02 12:54:27 -0600

    Renaming controller names

--- playing/backend-server-actix/src/endpoint_examples/files_endpoints/single_files.rs
+++ playing/backend-server-actix/src/endpoint_examples/files_endpoints/single_files.rs
@@ -3,12 +3,12 @@ use actix_web::get;
 use std::path::PathBuf;
 
 #[get("/")]
-pub async fn app_index() -> actix_web::Result<NamedFile> {
+pub async fn app_index_file() -> actix_web::Result<NamedFile> {
     let path = PathBuf::from("index.html");
     Ok(NamedFile::open(path)?)
 }
 
-pub async fn manual_index() -> actix_web::Result<NamedFile> {
+pub async fn manual_index_file() -> actix_web::Result<NamedFile> {
     let path = PathBuf::from("index.html");
     Ok(NamedFile::open(path)?)
 }
