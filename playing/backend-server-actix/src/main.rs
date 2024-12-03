13cc4c40f6f0b69667b9fedfd94f288422853d00
yasuomaidana <yasuomaidana@gmail.com>
2024-12-03 13:34:54 -0600

    Adding template paths to the server

--- playing/backend-server-actix/src/main.rs
+++ playing/backend-server-actix/src/main.rs
@@ -1,16 +1,18 @@
 mod endpoint_examples;
 mod states_example;
 mod utils;
+mod templates_example;
 
 use crate::endpoint_examples::scopes_example::files;
 use crate::endpoint_examples::simple_endpoints::*;
 use crate::states_example::states_endpoints::state_endpoints;
 use actix_web::{App, HttpServer};
+use crate::templates_example::templates_endpoint::templates_endpoint;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::builder()
-        .filter_level(log::LevelFilter::Info)
+        .filter_level(log::LevelFilter::Debug)
         .init();
     HttpServer::new(|| {
         App::new()
@@ -18,6 +20,7 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .service(state_endpoints())
             .service(files())
+            .service(templates_endpoint())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
