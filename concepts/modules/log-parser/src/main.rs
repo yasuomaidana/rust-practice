17735917cf695e784f5bdd3fd66a600d304c8f4a
yasuomaidana <yasuomaidana@gmail.com>
2024-12-13 18:24:19 -0600

    Calling function from main

--- concepts/modules/log-parser/src/main.rs
+++ concepts/modules/log-parser/src/main.rs
@@ -1,4 +1,13 @@
+mod parser;
+use parser::read_buffer;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: <path>");
+        std::process::exit(1);
+    }
+    // macOs usage example: cargo run --release -- /var/log/system.log
+    let path = &args[1];
+    read_buffer(path);
 }
