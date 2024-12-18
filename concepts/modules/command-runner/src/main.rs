176af1140d396df26840a3d55e07f9fb7a4d0f68
yasuomaidana <yasuomaidana@gmail.com>
2024-12-18 16:53:33 -0600

    Running console commands from rust

---
+++ concepts/modules/command-runner/src/main.rs
@@ -0,0 +1,10 @@
+use std::process::Command;
+fn main() {
+    let output = Command::new("ls")
+        .arg("-l")
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+}
